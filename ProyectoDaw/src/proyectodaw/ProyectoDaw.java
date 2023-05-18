fad9ce8dd11e98819cf82b0bc90b488202c0a805
jpingil <jpingil@DAW1-44.dip.local>
2023-05-18 12:29:22 +0200

    18/05/23	12:30

--- ProyectoDaw/src/proyectodaw/ProyectoDaw.java
+++ ProyectoDaw/src/proyectodaw/ProyectoDaw.java
@@ -15,6 +15,7 @@ public class ProyectoDaw {
      */
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("Lo primero de todo, ¿cómo están los máquinas?");
     }
-    
+
 }
